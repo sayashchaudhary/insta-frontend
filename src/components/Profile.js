e74b6e53f9688acd9f6fd6675b947f324f20aa62
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-29 18:20:48 +0530

    profile UI

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,9 +2,39 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <h1>
-            Profile
-        </h1>
+        <div style={{maxWidth: "70%", margin: "0px auto"}}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    margin: "18px 0", borderBottom: "1px solid grey"}}
+            >
+                <div>
+                    <img
+                        style={{width: "160px", height: "160px", borderRadius: "80px"}}
+                        src="https://images.unsplash.com/photo-1489980557514-251d61e3eeb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    />
+                </div>
+                <div>
+                    <h4>Sayash Chaudhary</h4>
+                    <div
+                        style={{display: "flex", justifyContent: "space-between", width: "108%"}}
+                    >
+                        <h5>40 posts</h5>
+                        <h5>40 followers</h5>
+                        <h5>40 following</h5>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+        </div>
     )
 };
 
